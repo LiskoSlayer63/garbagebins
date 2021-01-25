c76e90e45f86130eedc72c80643bcb3087c5cedf
LiskoSlayer63 <markus.ahola@hotmail.com>
2021-01-25 09:36:20 +0200

    Removed unused configs

--- src/main/java/lizcraft/garbagebins/GarbageBins.java
+++ src/main/java/lizcraft/garbagebins/GarbageBins.java
@@ -4,12 +4,9 @@ import org.apache.logging.log4j.LogManager;
 
 import lizcraft.garbagebins.client.ClientContent;
 import lizcraft.garbagebins.common.CommonContent;
-import lizcraft.garbagebins.config.GarbageBinsConfig;
 import lizcraft.garbagebins.utils.Logger;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(GarbageBins.MOD_ID)
@@ -22,9 +19,6 @@ public class GarbageBins
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		
 		Logger.init(LogManager.getLogger(GarbageBins.class));
-		
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, GarbageBinsConfig.CLIENT_CONFIG);
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, GarbageBinsConfig.COMMON_CONFIG);
 		
 		CommonContent.register(modEventBus);
 		ClientContent.register(modEventBus);
